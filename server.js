3e5855ed43ca846dbf06595a1f353f5a3f493bc1
rrgayhart <rrgayhart@gmail.com>
2017-03-14 13:42:24 -0600

    Set up base level boilerplate

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Extreme Pizza Box';
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+const port = process.env.PORT || 3000;
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+  });
+
+app.get('/', (request, response) => {
+  response.sendfile(__dirname + '/public/index.html');
+});
