77d9f66a3ae7f457bd79f3372ae08821ce55f287
rrgayhart <rrgayhart@gmail.com>
2017-03-16 09:03:52 -0600

    revert answers

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Extreme Pizza Box';
-app.locals.pizzas = [];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,16 +14,9 @@ app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/v1/pizzas', (request, response) => {
-  response.status(200).json(app.locals.pizzas);
-});
-
 app.use(express.static(__dirname + '/public'));
 
-if (!module.parent) {
-  app.listen(app.get('port'), () => {
-    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-  });
-}
 
-module.exports = app;
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
